6b3c45f802cee47e421c1c4acf9152219dbe7101
reyhubcom <94319232+reyhubcom@users.noreply.github.com>
2023-05-30 20:12:26 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'STAKING | WIKI Validator & Delagate Coins';
+export const SITE_TITLE = 'Staking | Wiki Validator & Delagate Coins';
 export const SITE_DESCRIPTION = 'Astro Modern Personal Website template with Project Section, CV Section, Paginated Blog, RSS Feed, SEO Friendly, Visual themes and Responsive Desing.';
